62a5482a843ba01160820972144153eb2b256245
mohamed_noor <mn2005308@gmail.com>
2024-01-22 14:07:30 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
